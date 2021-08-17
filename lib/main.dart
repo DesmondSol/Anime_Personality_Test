0800ac8a5728720e134bc533528ca16f819dad64
DesmondSol <soltig66@gmail.com>
2021-08-17 17:01:22 +0300

    debug tag is deleted

--- lib/main.dart
+++ lib/main.dart
@@ -216,6 +216,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
             title: const Text('Anime personality test'),
